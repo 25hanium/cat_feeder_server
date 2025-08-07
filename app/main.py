a78a9be2f56b7e1e88f6c152a5120e7f986ada8a
eastjae <111339770+eastjae@users.noreply.github.com>
2025-08-08 01:25:10 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -7,3 +7,11 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(feeding.router, prefix="/feeding", tags=["Feeding"])
+
+@app.post("/api/logs", response_model=schemas.FeedingLogResponse)
+def create_log(log: schemas.FeedingLogCreate, db: Session = Depends(get_db)):
+    return crud.create_log(db, log)
+
+@app.get("/api/logs/latest", response_model=schemas.FeedingLogResponse)
+def latest_log(db: Session = Depends(get_db)):
+    return crud.get_latest_log(db)
